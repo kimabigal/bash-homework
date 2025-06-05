efe0b90b631b6858acd641f12efa6ec2f9d4d24e
EC2 Default User <ec2-user@ip-172-31-90-94.ec2.internal>
2025-06-05 21:00:57 +0000

    Homework-12

---
+++ Homework-12/input.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+read -p "Enter your name: " name
+read -p "Enter your age: " age
+read -p "Enter your favorite programming language: " favorite_language
+read -p "Enter your hobby: " hobby
+read -p "Enter your dream in one word: " dream
+
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning,$name likes to spend time $hobby."
+echo "As a $age-year-old, I believe it's never too late to follow your dreams. My favorite programming language is $favorite_language. I code in $favorite_language almost every day!"
+echo "One day, I hope my love for $favorite_language will help me achieve my dream of $dream.  When I'm not busy working toward $dream, you'll find me $hobby to relax."
+echo "That's a little about me, $name — a $age-year-old who loves $favorite_language, enjoys $hobby, and dreams of $dream"
---
+++ Homework-12/introduce.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+name=Abigail
+age=30
+hobby=running
+favorite_language=Java
+dream="becoming a PO"
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning,$name likes to spend time $hobby."
+echo 
+echo "As a $age-year-old, I believe it's never too late to follow your dreams. My favorite programming language is $favorite_language. I code in $favorite_language almost every day!"
+echo
+echo "One day, I hope my love for $favorite_language will help me achieve my dream of $dream.  When I'm not busy working toward $dream, you'll find me $hobby to relax."
+echo
+echo "That's a little about me, $name — a $age-year-old who loves $favorite_language, enjoys $hobby, and dreams of $dream"
+!
